33a8f987fc8f5078b61c278971daa35e87dab138
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-17 17:04:59 +0200

    Refactor

--- src/spread.js
+++ src/spread.js
@@ -2,19 +2,12 @@ function spread(wholeFunction, args) {
   if (!wholeFunction || typeof wholeFunction != 'string') {
     throw new Error('missing Function!');
   }
-
-  console.log('args: ', args);
   const functionBody = wholeFunction.split(')')[1];
-  console.log('functionBody: ', functionBody);
   if (args) {
     const x = args[0] ? args[0] : 0;
     const y = args[1] ? args[1] : 0;
     const z = args[2] ? args[2] : 0;
-    console.log('x: ', x);
-    console.log('y: ', y);
-    console.log('z: ', z);
     let dynamicFunction = new Function('x', 'y', 'z', functionBody);
-    console.log('dynamicFunction: ', functionBody);
     return dynamicFunction(x, y, z);
   } else {
     let dynamicFunctionWithouArgs = new Function(functionBody);
